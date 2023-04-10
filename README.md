76eac5ec430d31af725422ea38b3ceed4f768677
Pigonhair <pigonhairtt@gmail.com>
2023-04-10 15:03:05 +0900

    ~JPA와 DB 설정, 동작확인

--- README.md
+++
@@ -1,4 +0,0 @@
-# JPA 정리 - Branch별 강의 분리
-
-## Java-ORM-Basic-JPA -> 자바 ORM 표준 JPA 프로그래밍
-## SpringBoot-JPA-1 -> 실전! 스프링 부트와 JPA 활용1
---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    // 스프링이 만들어둔 EntityManger를 주입받을 때 사용하는 어노테이션
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
